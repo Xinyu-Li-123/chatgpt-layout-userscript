dc02542c5d3154cd9a3f73a8eb4a44aafabbb7f7
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-05-29 04:12:18 -0400

    Add locale info

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,18 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        name: 'ChatGPT Conversation Width Sliders',
+        name: {
+          '': 'ChatGPT Conversation Width Sliders',
+          'zh-CN': 'ChatGPT 对话框宽度滑块',
+        },
         namespace: 'https://chatgpt.com/',
         version: '0.1.0',
         license: 'MIT',
-        description:
-          'Adjust the width and position of ChatGPT conversation content with some sliders.',
+        description: {
+          '':
+            'Adjust the width and position of ChatGPT conversation content with some sliders.',
+          'zh-CN': '调整 ChatGPT 对话框的宽度。',
+        },
         match: ['https://chatgpt.com/*', 'https://chat.openai.com/*'],
         grant: 'none',
       },
